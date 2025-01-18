a0851ad1aa783185736db07d38505789f975bb66
JagzModels <officialmusadurowoju@gmail.com>
2025-01-18 14:14:32 -0800

    SQL Queries

---
+++ Accenture_Task.sql
@@ -0,0 +1,274 @@
+-- Accenture task for month with most post 
+	
+	SELECT COUNT  ("DateTime") FROM
+			
+			"CONTENTz" c
+		JOIN
+			"REACTIONz" r ON c."Content ID" = r."Content ID"
+		JOIN
+			"REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+			WHERE "DateTime" LIKE '2020-12%';
+
+			SELECT  * FROM
+
+
+			
+			
+			"CONTENTz" c ;
+
+			SELECT DISTINCT "Category" FROM
+			
+			"CONTENTz" c ;
+
+			-- January 2020
+SELECT COUNT("DateTime") FROM
+    "CONTENTz" c
+JOIN
+    "REACTIONz" r ON c."Content ID" = r."Content ID"
+JOIN
+    "REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+WHERE "DateTime" LIKE '2020-01%';
+
+-- February 2020
+SELECT COUNT("DateTime") FROM
+    "CONTENTz" c
+JOIN
+    "REACTIONz" r ON c."Content ID" = r."Content ID"
+JOIN
+    "REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+WHERE "DateTime" LIKE '2020-02%';
+
+-- March 2020
+SELECT COUNT("DateTime") FROM
+    "CONTENTz" c
+JOIN
+    "REACTIONz" r ON c."Content ID" = r."Content ID"
+JOIN
+    "REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+WHERE "DateTime" LIKE '2020-03%';
+
+-- April 2020
+SELECT COUNT("DateTime") FROM
+    "CONTENTz" c
+JOIN
+    "REACTIONz" r ON c."Content ID" = r."Content ID"
+JOIN
+    "REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+WHERE "DateTime" LIKE '2020-04%';
+
+-- May 2020
+SELECT COUNT("DateTime") FROM
+    "CONTENTz" c
+JOIN
+    "REACTIONz" r ON c."Content ID" = r."Content ID"
+JOIN
+    "REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+WHERE "DateTime" LIKE '2020-05%';
+
+-- June 2020
+SELECT COUNT("DateTime") FROM
+    "CONTENTz" c
+JOIN
+    "REACTIONz" r ON c."Content ID" = r."Content ID"
+JOIN
+    "REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+WHERE "DateTime" LIKE '2020-06%';
+
+-- July 2020
+SELECT COUNT("DateTime") FROM
+    "CONTENTz" c
+JOIN
+    "REACTIONz" r ON c."Content ID" = r."Content ID"
+JOIN
+    "REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+WHERE "DateTime" LIKE '2020-07%';
+
+-- August 2020
+SELECT COUNT("DateTime") FROM
+    "CONTENTz" c
+JOIN
+    "REACTIONz" r ON c."Content ID" = r."Content ID"
+JOIN
+    "REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+WHERE "DateTime" LIKE '2020-08%';
+
+-- September 2020
+SELECT COUNT("DateTime") FROM
+    "CONTENTz" c
+JOIN
+    "REACTIONz" r ON c."Content ID" = r."Content ID"
+JOIN
+    "REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+WHERE "DateTime" LIKE '2020-09%';
+
+-- October 2020
+SELECT COUNT("DateTime") FROM
+    "CONTENTz" c
+JOIN
+    "REACTIONz" r ON c."Content ID" = r."Content ID"
+JOIN
+    "REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+WHERE "DateTime" LIKE '2020-10%';
+
+-- November 2020
+SELECT COUNT("DateTime") FROM
+    "CONTENTz" c
+JOIN
+    "REACTIONz" r ON c."Content ID" = r."Content ID"
+JOIN
+    "REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+WHERE "DateTime" LIKE '2020-11%';
+
+-- December 2020
+SELECT COUNT("DateTime") FROM
+    "CONTENTz" c
+JOIN
+    "REACTIONz" r ON c."Content ID" = r."Content ID"
+JOIN
+    "REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+WHERE "DateTime" LIKE '2020-12%';
+
+
+-- January 2021
+SELECT COUNT("DateTime") FROM
+    "CONTENTz" c
+JOIN
+    "REACTIONz" r ON c."Content ID" = r."Content ID"
+JOIN
+    "REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+WHERE "DateTime" LIKE '2021-01%';
+
+-- February 2021
+SELECT COUNT("DateTime") FROM
+    "CONTENTz" c
+JOIN
+    "REACTIONz" r ON c."Content ID" = r."Content ID"
+JOIN
+    "REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+WHERE "DateTime" LIKE '2021-02%';
+
+-- March 2021
+SELECT COUNT("DateTime") FROM
+    "CONTENTz" c
+JOIN
+    "REACTIONz" r ON c."Content ID" = r."Content ID"
+JOIN
+    "REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+WHERE "DateTime" LIKE '2021-03%';
+
+-- April 2021
+SELECT COUNT("DateTime") FROM
+    "CONTENTz" c
+JOIN
+    "REACTIONz" r ON c."Content ID" = r."Content ID"
+JOIN
+    "REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+WHERE "DateTime" LIKE '2021-04%';
+
+-- May 2021
+SELECT COUNT("DateTime") FROM
+    "CONTENTz" c
+JOIN
+    "REACTIONz" r ON c."Content ID" = r."Content ID"
+JOIN
+    "REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+WHERE "DateTime" LIKE '2021-05%';
+
+-- June 2021
+SELECT COUNT("DateTime") FROM
+    "CONTENTz" c
+JOIN
+    "REACTIONz" r ON c."Content ID" = r."Content ID"
+JOIN
+    "REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+WHERE "DateTime" LIKE '2021-06%';
+
+-- July 2021
+SELECT COUNT("DateTime") FROM
+    "CONTENTz" c
+JOIN
+    "REACTIONz" r ON c."Content ID" = r."Content ID"
+JOIN
+    "REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+WHERE "DateTime" LIKE '2021-07%';
+
+-- August 2021
+SELECT COUNT("DateTime") FROM
+    "CONTENTz" c
+JOIN
+    "REACTIONz" r ON c."Content ID" = r."Content ID"
+JOIN
+    "REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+WHERE "DateTime" LIKE '2021-08%';
+
+-- September 2021
+SELECT COUNT("DateTime") FROM
+    "CONTENTz" c
+JOIN
+    "REACTIONz" r ON c."Content ID" = r."Content ID"
+JOIN
+    "REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+WHERE "DateTime" LIKE '2021-09%';
+
+-- October 2021
+SELECT COUNT("DateTime") FROM
+    "CONTENTz" c
+JOIN
+    "REACTIONz" r ON c."Content ID" = r."Content ID"
+JOIN
+    "REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+WHERE "DateTime" LIKE '2021-10%';
+
+-- November 2021
+SELECT COUNT("DateTime") FROM
+    "CONTENTz" c
+JOIN
+    "REACTIONz" r ON c."Content ID" = r."Content ID"
+JOIN
+    "REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+WHERE "DateTime" LIKE '2021-11%';
+
+-- December 2021
+SELECT COUNT("DateTime") FROM
+    "CONTENTz" c
+JOIN
+    "REACTIONz" r ON c."Content ID" = r."Content ID"
+JOIN
+    "REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+WHERE "DateTime" LIKE '2021-12%';
+
+
+-- Number of reactions with respect to Sentiment by Category and Ordered by Count
+
+SELECT COUNT ( "Sentiment"), "Category", "Sentiment" FROM
+			
+			"CONTENTz" c
+		JOIN
+			"REACTIONz" r ON c."Content ID" = r."Content ID"
+		JOIN
+			"REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+			GROUP BY "Category", "Sentiment"
+			ORDER BY COUNT DESC ;
+			  ;
+
+			
+-- All From Joined Table
+			SELECT  * FROM "CONTENTz" c
+
+			JOIN
+			"REACTIONz" r ON c."Content ID" = r."Content ID"
+		JOIN
+			"REACTION TYPESz" rt ON rt."Type" = r."Reaction Type" ;
+
+
+
+-- Number of post BY Categories
+			SELECT "Category", COUNT ( "Category") FROM
+			
+			"CONTENTz" c
+		JOIN
+			"REACTIONz" r ON c."Content ID" = r."Content ID"
+		JOIN
+			"REACTION TYPESz" rt ON rt."Type" = r."Reaction Type"
+			GROUP BY "Category"
+			ORDER BY COUNT DESC ;
